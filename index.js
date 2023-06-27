463b657f46bf6b145d32c05753dac6a469d25a30
iamcal77 <+>
2023-06-27 10:57:49 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+// index.js
+
+// SaturdayFun function declaration
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+
+// MondayWork function expression
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+
+
+// WrapAdjective function
+function wrapAdjective(flair = "*") {
+    return function(adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+   
